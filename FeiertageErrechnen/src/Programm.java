5b0963f5c557153def297be8b49dbd8f52794b62
David <simma.david@gmail.com>
2020-09-29 20:23:27 +0200

    Programm neu struktureiert; interaktive eingabe der gewünschten Jahre möglich

--- FeiertageErrechnen/src/Programm.java
+++ FeiertageErrechnen/src/Programm.java
@@ -4,50 +4,28 @@ import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Programm {
+    public static List<LocalDate> feiertage = new ArrayList<>();
     public static void main(String[] args){
 
-        List<LocalDate> feiertage = new ArrayList<>();
-        int monday=0,tuesday=0,wednesday=0,thursday=0,friday=0,saturday=0,sunday=0;
-
+        Scanner reader = new Scanner(System.in);
+        int monday=0,tuesday=0,wednesday=0,thursday=0,friday=0,saturday=0,sunday=0, years;
         LocalDate k = LocalDate.now();
-        System.out.println(k.plusDays(1));
-        /*for (int i = 2020; i <= 2120; i++){
-            feiertage.add(LocalDate.of(i,1,1));
-            feiertage.add(LocalDate.of(i,6,1));
-            feiertage.add(LocalDate.of(i,5,1));
-            feiertage.add(LocalDate.of(i,10,26));
-            feiertage.add(LocalDate.of(i,11,1));
-            feiertage.add(LocalDate.of(i,12,8));
-            feiertage.add(LocalDate.of(i,12,25));
-            feiertage.add(LocalDate.of(i,12,26));
 
-        }
+
+        System.out.print("Wie viele Jahre sollen vorausgerechnet werden?: ");
+        years = reader.nextInt();
+        feiertageErzeugen(years);
+        gewuenschtenFeiertage();
+
+        for(int i = 0; i < feiertage.size(); i++){
+            //System.out.println(i);
 
 
 
-        System.out.println(feiertage);
-        System.out.println(LocalDate.now());
-
-        try (FileOutputStream fos = new FileOutputStream("feiertage.bin"); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-            oos.writeObject(feiertage);
-        } catch (IOException e) {
-            System.out.println("Etwas ist schief gelaufen! Bitte wenden Sie sich an den Callcenter-Support!");
-        }
-        */
-
-        try (FileInputStream fis = new FileInputStream("feiertage.bin"); ObjectInputStream ois = new ObjectInputStream(fis)) {
-            feiertage = (List<LocalDate>) ois.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            System.out.println("Serialisierung hat nicht funktioniert!");
-        }
-        System.out.println(feiertage);
-        for(LocalDate i = LocalDate.of(2020, 1, 1); i.isBefore(LocalDate.of(2020, 1, 2)); i.plusDays(1)){
-            System.out.println(i);
-            if (feiertage.contains(i)){
-
-                switch (i.getDayOfWeek()){
+                switch (feiertage.get(i).getDayOfWeek()){
                     case MONDAY:
                         monday+=1;
                         break;
@@ -72,7 +50,7 @@ public class Programm {
                     default:
                         break;
                 }
-            }
+
         }
 
         System.out.println("Montag: " + monday);
@@ -82,6 +60,40 @@ public class Programm {
         System.out.println("Freitag: " + friday);
         System.out.println("Samstag: " + saturday);
         System.out.println("Sonntag: " + sunday);
+
+    }
+
+    public static void feiertageErzeugen(int years){
+
+        for (int i = 2020; i <= 2020+years; i++){
+            feiertage.add(LocalDate.of(i,1,1));
+            feiertage.add(LocalDate.of(i,6,1));
+            feiertage.add(LocalDate.of(i,5,1));
+            feiertage.add(LocalDate.of(i,10,26));
+            feiertage.add(LocalDate.of(i,11,1));
+            feiertage.add(LocalDate.of(i,12,8));
+            feiertage.add(LocalDate.of(i,12,25));
+            feiertage.add(LocalDate.of(i,12,26));
+
+        }
+
+        try (FileOutputStream fos = new FileOutputStream("feiertage.bin"); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(feiertage);
+        } catch (IOException e) {
+            System.out.println("Etwas ist schief gelaufen! Bitte wenden Sie sich an den Callcenter-Support!");
+        }
+        feiertage.clear();
+    }
+
+    public static void gewuenschtenFeiertage(){
+
+        try (FileInputStream fis = new FileInputStream("feiertage.bin"); ObjectInputStream ois = new ObjectInputStream(fis)) {
+            feiertage = (List<LocalDate>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Serialisierung hat nicht funktioniert!");
+        }
+
+
 
     }
 }
