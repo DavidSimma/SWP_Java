ab18600496f9c6fa4deab28c0bd139cf3af00631
David <simma.david@gmail.com>
2020-09-30 12:34:55 +0200

    Mann kann Feiertage extern in txt eingeben und diese werden ausgelesen

--- FeiertageErrechnen/src/Programm.java
+++ FeiertageErrechnen/src/Programm.java
@@ -3,11 +3,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
 public class Programm {
     public static List<LocalDate> feiertage = new ArrayList<>();
+    public static List<String> nix = new ArrayList<>();
     public static void main(String[] args){
 
         Scanner reader = new Scanner(System.in);
@@ -17,15 +19,12 @@ public class Programm {
 
         System.out.print("Wie viele Jahre sollen vorausgerechnet werden?: ");
         years = reader.nextInt();
+
+        feiertageEinlesen();
         feiertageErzeugen(years);
-        gewuenschtenFeiertage();
 
         for(int i = 0; i < feiertage.size(); i++){
-            //System.out.println(i);
-
-
-
-                switch (feiertage.get(i).getDayOfWeek()){
+            switch (feiertage.get(i).getDayOfWeek()){
                     case MONDAY:
                         monday+=1;
                         break;
@@ -66,34 +65,26 @@ public class Programm {
     public static void feiertageErzeugen(int years){
 
         for (int i = 2020; i <= 2020+years; i++){
-            feiertage.add(LocalDate.of(i,1,1));
-            feiertage.add(LocalDate.of(i,6,1));
-            feiertage.add(LocalDate.of(i,5,1));
-            feiertage.add(LocalDate.of(i,10,26));
-            feiertage.add(LocalDate.of(i,11,1));
-            feiertage.add(LocalDate.of(i,12,8));
-            feiertage.add(LocalDate.of(i,12,25));
-            feiertage.add(LocalDate.of(i,12,26));
+            for(int o = 0; o<nix.size(); o++) {
+                String[] temp = new String[2];
+                temp = nix.get(o).split(",");
+                feiertage.add(LocalDate.of(i, Integer.parseInt(temp[1]), Integer.parseInt(temp[0])));
+            }
+
 
         }
-
-        try (FileOutputStream fos = new FileOutputStream("feiertage.bin"); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-            oos.writeObject(feiertage);
-        } catch (IOException e) {
-            System.out.println("Etwas ist schief gelaufen! Bitte wenden Sie sich an den Callcenter-Support!");
-        }
-        feiertage.clear();
     }
+    public static void feiertageEinlesen(){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("feiertage.txt"));
+            String temp = br.readLine();
+            while(temp != null){
+                nix.add(temp);
+                temp = br.readLine();
 
-    public static void gewuenschtenFeiertage(){
-
-        try (FileInputStream fis = new FileInputStream("feiertage.bin"); ObjectInputStream ois = new ObjectInputStream(fis)) {
-            feiertage = (List<LocalDate>) ois.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            System.out.println("Serialisierung hat nicht funktioniert!");
+            }
+        }catch (IOException e){
+            System.out.println("Feiertage konnten nicht eingelesen werden");
         }
-
-
-
     }
 }
