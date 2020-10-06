4a0deb8b4d161774fe138d674a8d4639a306e2fb
David <simma.david@gmail.com>
2020-10-06 20:49:20 +0200

    Feiertage werden mittels API und txt eingelesen und errechnet

--- FeiertageErrechnen/src/Programm.java
+++ FeiertageErrechnen/src/Programm.java
@@ -1,30 +1,32 @@
+import org.json.JSONObject;
+
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Scanner;
+import org.apache.commons.io.IOUtils;
+import java.util.*;
 
 public class Programm {
-    public static List<LocalDate> feiertage = new ArrayList<>();
+    public static HashMap<LocalDate, String> feiertage = new HashMap();
     public static List<String> nix = new ArrayList<>();
     public static void main(String[] args){
 
         Scanner reader = new Scanner(System.in);
-        int monday=0,tuesday=0,wednesday=0,thursday=0,friday=0,saturday=0,sunday=0, years;
+        int monday=0,tuesday=0,wednesday=0,thursday=0,friday=0,saturday=0,sunday=0, anfangsJahr, endjahr;
         LocalDate k = LocalDate.now();
 
 
-        System.out.print("Wie viele Jahre sollen vorausgerechnet werden?: ");
-        years = reader.nextInt();
+        System.out.print("Anfangsjahr: ");
+        anfangsJahr = reader.nextInt();
+        System.out.print("Endjahr: ");
+        endjahr = reader.nextInt();
 
-        feiertageEinlesen();
-        feiertageErzeugen(years);
+        feiertageErzeugen(anfangsJahr, endjahr);
 
-        for(int i = 0; i < feiertage.size(); i++){
-            switch (feiertage.get(i).getDayOfWeek()){
+        for(LocalDate ld : feiertage.keySet()){
+            switch (ld.getDayOfWeek()){
                     case MONDAY:
                         monday+=1;
                         break;
@@ -40,12 +42,6 @@ public class Programm {
                     case FRIDAY:
                         friday+=1;
                         break;
-                    case SATURDAY:
-                        saturday+=1;
-                        break;
-                    case SUNDAY:
-                        sunday+=1;
-                        break;
                     default:
                         break;
                 }
@@ -57,22 +53,50 @@ public class Programm {
         System.out.println("Mittwoch: " + wednesday);
         System.out.println("Donnerstag: " + thursday);
         System.out.println("Freitag: " + friday);
-        System.out.println("Samstag: " + saturday);
-        System.out.println("Sonntag: " + sunday);
 
     }
 
-    public static void feiertageErzeugen(int years){
-
-        for (int i = 2020; i <= 2020+years; i++){
+    public static void feiertageErzeugen(int anfang, int ende){
+        feiertageEinlesen();
+        for (int i = anfang; i <= ende; i++){
             for(int o = 0; o<nix.size(); o++) {
-                String[] temp = new String[2];
+                String[] temp = new String[3];
+                LocalDate temp2;
                 temp = nix.get(o).split(",");
-                feiertage.add(LocalDate.of(i, Integer.parseInt(temp[1]), Integer.parseInt(temp[0])));
+                temp2 = LocalDate.of(i, Integer.parseInt(temp[1]), Integer.parseInt(temp[0]));
+                feiertage.put(temp2, temp[2].toString());
+
             }
+            feiertageEinlesen(i, "Ostermontag");
+            feiertageEinlesen(i, "Christi Himmelfahrt");
+            feiertageEinlesen(i, "Pfingstmontag");
+            feiertageEinlesen(i, "Fronleichnam");
 
 
         }
+
+    }
+    public static JSONObject APIEinlesen(String url){
+        JSONObject json = new JSONObject();
+
+
+
+        try {
+                json = new JSONObject(IOUtils.toString(new URL(url), Charset.forName("UTF-8")));
+            }
+        catch (MalformedURLException e){
+            System.out.println("URL funktioniert nicht");
+        }catch (IOException e){
+            System.out.println("Feiertage konnten nicht eingelesen werden");
+        }
+        return json;
+    }
+
+    private static void feiertageEinlesen(int year, String name) {
+        String urlBase = "https://feiertage-api.de/api/?jahr=";
+        JSONObject json = APIEinlesen(urlBase + year);
+        String date = json.getJSONObject("BY").getJSONObject(name).get("datum").toString();
+        feiertage.put(LocalDate.parse(date), name);
     }
     public static void feiertageEinlesen(){
         try {
@@ -87,4 +111,5 @@ public class Programm {
             System.out.println("Feiertage konnten nicht eingelesen werden");
         }
     }
+
 }
